4fbc39216d8ec2eb7722520e5864ed917979fb70
crz <crz1336768325@163.com>
2021-03-12 16:58:23 +0800

    userMangement

---
+++ frontend/js_src/userMangement.js
@@ -0,0 +1,39 @@
+
+//用户登录界面的设计
+window.onload=function(){
+    var vm = new Vue({
+        el:"#loginpage",
+        data:{
+            username:"",
+            pw:""
+        },
+        methods : {
+
+            submit_user_passward:function(){
+
+                console.log("提交用户名");
+                console.log("用户名"+this.username)
+                console.log(this.pw)
+                var url="http://127.0.0.1:8000/blog/login/"
+                // 进行post 请求            
+                axios.post(url,{"name":this.username,'password':this.pw}).then(function (response) {
+                    console.log(response.data['status'])
+                    if (response.data['status']==="success"){
+                        console.log("成功登录")
+                        // alert("成功登录")
+                        // location.href = "http://www.baidu.com"; //重定向到新的页面
+                        location.href = "../html/imageInference.html"; 
+
+                    }else{
+                        console.log("登录失败，用户名或密码不正确")
+                        alert("登录失败，用户名或密码不正确")
+                        // location.href = "https://www.runoob.com/"; //重定向成功
+                    }
+                }).catch(function (error) {
+                    console.log(error);
+                });
+                // alert("show something");
+            }
+        }
+    })
+}
